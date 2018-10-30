7e8dd124efc43719d775b1c0b074f25043dce800
Horyzont <s4664@horyzont.eu>
2018-10-30 13:50:18 +0100

    Expressions

---
+++ Expressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Variable.Collection.Add("x", 1.2);
+            
+            Expression n1 = new Sum(new Overload(new Number(2.25)), new Number(3.75) );
+            Console.WriteLine(n1);
+
+            Console.ReadKey();
+        }
+    }
+}
